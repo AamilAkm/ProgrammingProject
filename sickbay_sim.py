e721188ff949f815f0b776e1d3e59285d4d6ff4d
AamilAk <162271189+AamilAk@users.noreply.github.com>
2026-06-01 10:48:15 +0530

    Add initial implementation of sickbay simulation

---
+++ sickbay_sim.py
@@ -0,0 +1,13 @@
+import simpy
+import random
+
+#Scenario – A school with limited number of nurses. 
+
+#Variables – number of nurses | wait time in sickbay | students in queue | urgent cases | nurses occupied with an urgent case
+
+def sickbay(env, name, nurses):
+    None
+
+def student_generator(env, nurses):
+    None
+
