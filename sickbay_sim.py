4c6b62334ac71ffbef6a268dfbdc541c2eb881b9
AamilAk <162271189+AamilAk@users.noreply.github.com>
2026-06-03 20:45:46 +0530

    Implemented write and save to text file for summary

--- sickbay_sim.py
+++ sickbay_sim.py
@@ -21,37 +21,51 @@ class sickbay_simulation:
     def __init__(self, env, num_nurses):
         self.env = env
         self.nurses = simpy.PriorityResource(env, capacity=num_nurses)
-        self.simulation_summary = ''
+        self.urgent_case_id = 1
+        self.student_id = 1
         self.untreated_students = 0
         self.treated_students = 0
+
+        self.summary_file = open('simulation_summary.txt', 'w') #Save Simulation summary
 
         self.env.process(self.student_generator())
         self.env.run()
 
 
-    def print_outcome(self, student):
+    def simulation_summary(self, student):
+        text = self.summary_file
 
-        self.simulation_summary += f'\n  PATIENT : {student.name}'
+        patient_text = f"PATIENT - {student.name}"
+        text.write(f'\n{patient_text.center(60, "=")}\n')
 
-        # Timeline
-        self.simulation_summary += f'\n  Timeline:'
-        self.simulation_summary += f'\n   Arrival:        {student.arrival_time:7.1f}'
-        self.simulation_summary += f'\n   Wait Time:      {student.wait_time:7.1f}'
-        self.simulation_summary += f'\n   Departure:      {student.leave_time:7.1f}'
+        text.write(f'\n{f'Timeline':-^60}')
+        text.write(f'\n   Arrival{" "*45}{student.arrival_time}')
+        text.write(f'\n   Wait Time:{" "*42}{student.wait_time}')
+        text.write(f'\n   Departure:{" "*42}{student.leave_time}')
 
         # Status
-        self.simulation_summary += f'\n' + '-' * 40
-        self.simulation_summary += f'\n  Status:'
+        text.write(f'\n{'Status':-^60}')
         if student.treated:
-            self.simulation_summary += f'\n  Successfully treated'
-            self.simulation_summary += f'\n   Attended at:        {student.arrival_time + student.wait_time:7.1f}'
-            self.simulation_summary += f'\n   Treatment duration: {student.leave_time - (student.arrival_time + student.wait_time):7.1f}'
+            text.write(f'\n   Successfully treated')
+            text.write(f'\n   Attended at:{" "*40}{student.arrival_time + student.wait_time}')
+            text.write(f'\n   Treatment duration:{" "*33}{student.leave_time - (student.arrival_time + student.wait_time)}')
         else:
-            self.simulation_summary += f'\n   Did not receive treatment (timeout)'
-            self.simulation_summary += f'\n   Max wait time exceeded: {student.wait_time}'
+            text.write(f'\n   Did not receive treatment (timeout)')
+            text.write(f'\n   Max wait time exceeded:{" "*29}{student.wait_time}')
+            
+        text.write(f'\n' + '-' * 60)
+        text.write(f'\n\n' + '=' * 60 + '\n')
+    
+    def read_summary(self):
+        file = open('simulation_summary.txt', 'r')
+        line = file.readline()
+        while line != '':
+            print(line)
+            line = file.readline()
+            
+        file.close()
         
-        self.simulation_summary += f'\n' + '=' * 60 + '\n'
-
+        
 
     def print_status(self, student, status):
         if status == 'arrived':
@@ -91,30 +105,33 @@ class sickbay_simulation:
                 self.untreated_students += 1
                 self.print_status(student, 'left')
             
-        self.print_outcome(student)
+        self.simulation_summary(student)
 
     def student_generator(self):
-        urgent_case_id = 1
-        student_id = 1
 
         while True:
             urgent_random = random.randint(1, 10)
             if urgent_random == 7:  # 10% chance of an urgent case
-                sick_student = student(f'Urgent Case {urgent_case_id}', urgent_case_id, priority=0)
+                sick_student = student(f'Urgent Case {self.urgent_case_id}', self.urgent_case_id, priority=0)
                 self.env.process(self.sickbay(sick_student))  # Process urgent case
-                urgent_case_id += 1
+                self.urgent_case_id += 1
             else:
                 yield self.env.timeout(random.randint(1, 3))  # Time between student arrivals
-                sick_student = student(f'Student {student_id}', student_id, priority=1)
+                sick_student = student(f'Student {self.student_id}', self.student_id, priority=1)
                 self.env.process(self.sickbay(sick_student))  # Pass the student object
-                student_id += 1
-                if student_id > 25:  # Limit the number of students for the simulation
+                self.student_id += 1
+                if self.student_id > 25:  # Limit the number of students for the simulation
                     break
-
+    
+    def print_summary(self):
+        user_input = input('\nDo you want to see the simulation summary? (yes/no): ')
+        if user_input.lower() == 'yes':
+            self.read_summary()
 
 sickbay = sickbay_simulation(simpy.Environment(), num_nurses=2)
-print(sickbay.simulation_summary)
+sickbay.print_summary()
 
 print(f'\nTotal students: {sickbay.treated_students + sickbay.untreated_students}')
+print(f'Total urgent cases: {sickbay.urgent_case_id - 1}')
 print(f'Treated students: {sickbay.treated_students}')
 print(f'Untreated students: {sickbay.untreated_students}')
